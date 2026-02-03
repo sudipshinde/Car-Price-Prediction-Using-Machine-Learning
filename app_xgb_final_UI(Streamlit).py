7f5f79079fe7d7314510e5e1641528ee01577c2d
Sudip <159097145+sudipshinde@users.noreply.github.com>
2026-02-03 08:49:12 +0530

    Add files via upload

--- app_xgb_final_UI(Streamlit).py
+++ app_xgb_final_UI(Streamlit).py
@@ -150,4 +150,4 @@ if page == "Car Price Prediction":
 # =================================================
 elif page == "Dashboard":
     st.subheader("📊 Analytics Dashboard")
-    webbrowser.open_new_tab("https://YOUR_DASHBOARD_LINK")
+    webbrowser.open_new_tab("https://public.tableau.com/views/Book1_17700872354460/Dashboard1?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link")
